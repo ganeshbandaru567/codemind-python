69db9de68d11504d8615fb87e989a3da39c64729
ganeshbandaru567 <99868339+ganeshbandaru567@users.noreply.github.com>
2023-02-02 20:37:26 +0530

    def is_disarium(num):     temp = 0     for i in range(len(str(num))):         temp += int(str(num)[i]) ** (i + 1)     return temp == num
    num=int(input())
    print(is_disarium(num))

---
+++ Disarium_number.py
@@ -0,0 +1,10 @@
+def is_disarium(num):
+    temp = 0
+    for i in range(len(str(num))):
+        temp += int(str(num)[i]) ** (i + 1)
+    return temp == num
+
+
+
+num=int(input())
+print(is_disarium(num))
