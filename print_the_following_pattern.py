b2c3ead0f78254eb96423a2e22267cf7a2551257
ganeshbandaru567 <99868339+ganeshbandaru567@users.noreply.github.com>
2023-02-02 20:17:50 +0530

    n=int(input()) for i in range(1,n+1):     for j in range(1,i+1):         print(j,end='')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,8 +1,5 @@
-ascii_number = 65
-rows=int(input())
-for i in range(0, rows):
-    for j in range(0, rows):
-        character = chr(ascii_number)
-        print(character, end=' ')
-    ascii_number += 1
+n=int(input())
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end='')
     print()
