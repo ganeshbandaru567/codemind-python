d0d220ff23dc69cbfe5d414e59f062043e747706
ganeshbandaru567 <99868339+ganeshbandaru567@users.noreply.github.com>
2023-06-05 12:12:29 +0530

    import math def prime(n):     if n==1:         return False     s=int(math.sqrt(n))     for i in range(2,s+1):         if n%i==0:             return False     else:         return True a=int(input()) b=int(input()) count=0 for i in range(a,b+1):     if prime(i):         count+=1 print(count)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,13 +1,17 @@
-def pir(num):
-    for i in range(2,int(num**0.5)+1):
-        if num%i==0:
-            return 0
-    return 1
+import math
+def prime(n):
+    if n==1:
+        return False
+    s=int(math.sqrt(n))
+    for i in range(2,s+1):
+        if n%i==0:
+            return False
+    else:
+        return True
 a=int(input())
 b=int(input())
-c=0
-for j in range(a,b+1):
-    if (pir(j) and j!=1):
-        c+=1
-print(c)
-        
+count=0
+for i in range(a,b+1):
+    if prime(i):
+        count+=1
+print(count)
