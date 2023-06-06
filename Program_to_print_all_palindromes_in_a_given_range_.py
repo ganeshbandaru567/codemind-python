785b5e07118c6ab6f0bec456e58c3967071115b1
ganeshbandaru567 <99868339+ganeshbandaru567@users.noreply.github.com>
2023-06-06 13:24:34 +0530

    def pal(n):     s=str(n)     l=s[::-1]     if s==l:         return True     else:         return False a=int(input()) b=int(input()) for i in range(a,b+1):     if pal(i):         print(i,end=' ')

--- Program_to_print_all_palindromes_in_a_given_range_.py
+++ Program_to_print_all_palindromes_in_a_given_range_.py
@@ -1,16 +1,12 @@
 def pal(n):
     s=str(n)
-    a=s[::-1]
-    if s==a:
+    l=s[::-1]
+    if s==l:
         return True
     else:
         return False
-
-
-
 a=int(input())
 b=int(input())
-count=0
 for i in range(a,b+1):
     if pal(i):
         print(i,end=' ')
